6e43d3c6468cf80f82e975389ed850e88bde9c68
Andrew Kelley <andrew@ziglang.org>
2021-11-01 16:29:35 -0700

    update translate-c benchmark; delete behavior/std test benchmarks

--- benchmarks/translate-c/main.zig
+++ benchmarks/translate-c/main.zig
@@ -2,18 +2,14 @@ const std = @import("std");
 const bench = @import("root");
 
 pub fn setup(gpa: *std.mem.Allocator, options: *bench.Options) ![]const u8 {
-    options.rusage_who = std.os.RUSAGE_CHILDREN;
+    _ = gpa;
+    options.rusage_who = std.os.rusage.CHILDREN;
     return options.zig_exe;
 }
 
 pub fn run(gpa: *std.mem.Allocator, zig_exe: []const u8) !void {
     return bench.exec(gpa, &[_][]const u8{
-        zig_exe,
-        "translate-c",
-        "input.h",
-        "-target",
-        "x86_64-windows-gnu",
-        "-lc",
+        zig_exe, "translate-c", "input.h", "-target", "x86_64-windows-gnu", "-lc",
     }, .{
         .stdout_behavior = .Ignore,
     });
